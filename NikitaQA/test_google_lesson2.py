302bd94c5733b48823778f90f2c3edd27ddb7d7c
Nikita <nikitazhuravlevqa@gmail.cm>
2024-03-11 00:54:31 +0300

    Добавил позитивный тест

--- NikitaQA/test_google_lesson2.py
+++ NikitaQA/test_google_lesson2.py
@@ -4,3 +4,8 @@ from selene import browser, have
 def browser_settings():
     browser.config.window_width = 1000
     browser.config.window_height = 1000
+
+def test_google_positive(browser_settings):
+    browser.open("https://www.google.com/")
+    browser.element("#APjFqb").type("yashaka/selene").press_enter()
+    browser.element("[id=search]").should(have.text("yashaka/selene: User-oriented Web UI browser"))
