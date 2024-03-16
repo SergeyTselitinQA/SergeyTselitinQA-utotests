654a1b94fa405ef412a132564f09f470880ddcbc
SergeyTselitinQA <sergeytselitinqa@gmail.com>
2024-03-16 14:47:36 +0300

    sergerLessonThree

--- SergeyQA/SergeyQA_1/sergeyTest.py
+++ SergeyQA/SergeyQA_1/sergeyTest.py
@@ -9,7 +9,4 @@ def test_can_login():
     browser.element('.login-form [name=password]').type('2fedaadd').press_enter()
     browser.element('#xdget642385_1').should(have.text('Здравствуйте, Vitali'))
     
-    
-    
-    
-    
+    browser.open('https://school.qa.guru/cms/system/logind')
