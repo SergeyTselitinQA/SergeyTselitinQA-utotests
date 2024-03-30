e33ff005e25e21dcc6a94e1c538a5c0822ad290d
SergeyTselitinQA <sergeytselitinqa@gmail.com>
2024-03-31 00:13:08 +0300

    seleneFixture

---
+++ SergeyQA/SergeyQA_5/conftest.py
@@ -0,0 +1,8 @@
+import pytest
+from selene import browser
+
+@pytest.fixture
+def open_browser():
+    browser.config.window_width = 1920
+    browser.config.window_height = 1080
+    browser.open('https://demoqa.com/automation-practice-form')
--- SergeyQA/SergeyQA_5/seleneFive.py
+++ SergeyQA/SergeyQA_5/seleneFive.py
@@ -2,5 +2,5 @@ from selene import browser
 
 browser.config.timeout =  8.0
 
-def test_hw():
-    browser.open('https://demoqa.com/automation-practice-form')
+def test_hw(open_browser):
+    #Тест готов к написанию
