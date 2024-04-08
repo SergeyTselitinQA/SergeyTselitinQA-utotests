983181e7ff41b5ffaef0bbead32fd0c8614c0e7a
SergeyTselitinQA <sergeytselitinqa@gmail.com>
2024-04-09 00:22:58 +0300

    fivestart

--- SergeyQA/SergeyQA_5/seleneFive.py
+++ SergeyQA/SergeyQA_5/seleneFive.py
@@ -1,6 +1,14 @@
-from selene import browser
+from selene import browser, have
 
-browser.config.timeout =  8.0
+browser.config.browser_name = "firefox"
+browser.config.hold_browser = True
 
-def test_hw(open_browser):
-    #Тест готов к написанию
+def test_hw():
+    browser.open('https://demoqa.com/automation-practice-form')
+
+    # WHEN
+    browser.element('#firstName').type('Ivan')
+    browser.element('#lastName').type('Ivanov')
+    browser.element('#userEmail').type('qaguru@mailto.plus')
+    browser.all('.custom-radio').element_by(have.text('Female')).click()
+    browser.element('#userNumber').type('+375')
