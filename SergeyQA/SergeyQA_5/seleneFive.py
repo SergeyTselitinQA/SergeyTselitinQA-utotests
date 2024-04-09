1ecf59d5d9d317a6fe6413df5a442c7c3c70b721
SergeyTselitinQA <sergeytselitinqa@gmail.com>
2024-04-09 19:51:22 +0300

    Ответ Никитосу

--- SergeyQA/SergeyQA_5/seleneFive.py
+++ SergeyQA/SergeyQA_5/seleneFive.py
@@ -1,7 +1,9 @@
-from selene import browser, have
+import time
+
+from selene import browser, have, command
 
 browser.config.browser_name = "firefox"
-browser.config.hold_browser = True
+browser.config.hold_browser_open = True
 
 def test_hw():
     browser.open('https://demoqa.com/automation-practice-form')
@@ -12,3 +14,6 @@ def test_hw():
     browser.element('#userEmail').type('qaguru@mailto.plus')
     browser.all('.custom-radio').element_by(have.text('Female')).click()
     browser.element('#userNumber').type('+375')
+    browser.element(".subjects-auto-complete__value-container").perform(command.js.scroll_into_view)
+    browser.element('#subjectsInput').type('English')
+    browser.element('#react-select-2-option-0').click()
