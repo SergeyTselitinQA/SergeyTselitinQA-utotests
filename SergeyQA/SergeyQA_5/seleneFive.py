2e3067db76d5916305e90c45fb325b053a1efd47
SergeyTselitinQA <sergeytselitinqa@gmail.com>
2024-04-12 00:14:04 +0300

    sergeyFiveFinishCher

--- SergeyQA/SergeyQA_5/seleneFive.py
+++ SergeyQA/SergeyQA_5/seleneFive.py
@@ -1,7 +1,4 @@
-from imghdr import tests
 from pathlib import Path
-
-
 from selene import browser, have, command
 
 browser.config.browser_name = "firefox"
@@ -16,7 +13,7 @@ def test_hw():
     browser.element('#lastName').type('Ivanov')
     browser.element('#userEmail').type('qaguru@mailto.plus')
     browser.all('.custom-radio').element_by(have.text('Female')).click()
-    browser.element('#userNumber').type('+375')
+    browser.element('#userNumber').type('3753333333')
 
     browser.element('#dateOfBirthInput').click()
     browser.element('.react-datepicker__month-select').type('May')
@@ -30,6 +27,22 @@ def test_hw():
     browser.element("[for='hobbies-checkbox-3']").perform(command.js.scroll_into_view)
     browser.element("[for='hobbies-checkbox-3']").click()
 
+    browser.element('#uploadPicture').perform(command.js.scroll_into_view)
     browser.element('#uploadPicture').set_value(
-        str(Path(tests.__file__).parent.joinpath('resources/foto.jpg').absolute())
+        str(Path(__file__).parent.joinpath('resources/foto.jpg').absolute())
     )
+
+    browser.element('#currentAddress').type('Беларусь, г. Орша, ул. Мира, д. 3')
+
+    browser.element('#state').perform(command.js.scroll_into_view)
+    browser.element('#state').click()
+    browser.all('[id^=react-select][id*=option]').element_by(
+        have.exact_text('NCR')
+    ).click()
+
+    browser.element('#city').click()
+    browser.all('[id^=react-select][id*=option]').element_by(
+        have.exact_text('Delhi')
+    ).click()
+
+    browser.element('#submit').click()
